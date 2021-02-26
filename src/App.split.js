a460ce8f944c10987645e3bdcf770a4b47629817
Micah Silverman <micah@afitnerd.com>
2021-02-26 12:23:20 -0500

    update package.json for minimum requirements. Split out different versions of App.js for use in the workshop.

---
+++ src/App.split.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { movies } from './movies.js'
+import MovieList from './MovieList.js'
+import { SplitFactory, SplitTreatments } from '@splitsoftware/splitio-react';
+
+// Filter that accepts only USA movies
+const usaFilter = (movie) => movie.country === 'USA';
+
+// Filter that accepts all movies
+const allMoviesFilter = () => true;
+
+export const localhostSplitConfig = {
+  core: {
+    authorizationKey: 'localhost',
+    key: 'anonymous_user',
+  },
+  features: {
+    'movie_filter': 'default',
+  }
+}
+
+export default function App() {
+
+  const [useDefaultFilter, setDefaultFilter] = useState(true);
+
+  return (
+    <SplitFactory config={localhostSplitConfig} updateOnSdkUpdate={true} >
+      <SplitTreatments /* names: list of features to evaluate */ names={['movie_filter']} >{
+        ({ isReady, treatments }) => {
+          if (isReady) {
+            // once the SDK is ready, `treatments` contains valid values of the evaluated list of features
+
+            // set the default filter to use based on the checkbox
+            let filter = useDefaultFilter ? allMoviesFilter : usaFilter;
+
+            // if the treatment value is 'USA', we use the USA filter instead of the default one.
+            if (treatments['movie_filter'].treatment === 'USA') filter = usaFilter;
+
+            const filteredMovies = movies.filter(filter);
+            return (
+              <div>
+                <div>
+                  <input type="checkbox" id="filter" checked={useDefaultFilter} onChange={() => { setDefaultFilter(!useDefaultFilter) }} />
+                  <label htmlFor="filter">Show International Movies</label>
+                </div>
+                <MovieList movies={filteredMovies} />
+              </div>
+            );
+          }
+
+          return (<div>Loading SDK...</div>);
+        }
+      }</SplitTreatments>
+    </SplitFactory>
+  )
+}
